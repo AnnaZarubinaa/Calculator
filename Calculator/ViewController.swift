9c5af3522a8fef1bff00773e62ba5739b6da61ec
Mike <82389159+AnnaZarubinaa@users.noreply.github.com>
2021-06-13 13:52:55 +0300

    created ui

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Mike MAC on 12.06.2021.
+//  Copyright © 2021 Mike MAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
